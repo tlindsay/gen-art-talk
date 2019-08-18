32cc654482279fdcc156bfddc899e626e8d9a356
Patrick Lindsay <patrick.lindsay@dockyard.com>
2019-08-18 14:42:23 -0500

    Add some more info to Intro and tweak spacing

--- src/presentation/01-intro.js
+++ src/presentation/01-intro.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { IconContext } from 'react-icons';
 import { FaTwitter, FaGithub, FaInternetExplorer } from 'react-icons/fa';
+import { DiEmber } from 'react-icons/di';
+import Narwin from '../assets/DY-Light BG.svg';
 
 import {
   Slide,
@@ -12,27 +15,42 @@ export default class IntroSlide extends React.Component {
   render() {
     return (
       <Slide {...this.props}>
-        <Heading
-          margin="0 auto 1em"
-          textColor="secondary"
-        >
-          Generative Art with WebGL
-        </Heading>
-        <div>
-          <Link textColor="tertiary" href="https://twitter.com/thatdarnpat">
-            <FaTwitter /> <S type="underline">@thatdarnpat</S>
-          </Link>
-        </div>
-        <div>
-          <Link textColor="dark" href="https://github.com/tlindsay">
-            <FaGithub /> <S type="underline">@tlindsay</S>
-          </Link>
-        </div>
-        <div>
-          <Link textColor="tertiary" href="https://twitter.com/thatdarnpat">
-            <FaInternetExplorer /> <S type="underline">thatdarnpat.com</S>
-          </Link>
-        </div>
+        <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
+          <Heading
+            margin="0 auto .2em"
+            textColor="secondary"
+          >
+            Generative Art with WebGL
+          </Heading>
+          <Heading
+            margin="0 auto 1em"
+            size={6}
+          >
+            Patrick Lindsay
+          </Heading>
+          <div style={{ textAlign: 'justify', margin: '0 auto', width: '300px' }}>
+            <div>
+              <Link textColor="tertiary" href="https://twitter.com/thatdarnpat">
+                <FaTwitter /> <S type="underline">@thatdarnpat</S>
+              </Link>
+            </div>
+            <div>
+              <Link textColor="dark" href="https://github.com/tlindsay">
+                <FaGithub /> <S type="underline">@tlindsay</S>
+              </Link>
+            </div>
+            <div>
+              <Link textColor="tertiary" href="https://twitter.com/thatdarnpat">
+                <FaInternetExplorer /> <S type="underline">thatdarnpat.com</S>
+              </Link>
+            </div>
+            <div style={{ marginTop: '1em', textAlign: 'center' }}>
+              <Link textColor="dark" href="https://dockyard.com">
+                <DiEmber size="60" color="#e04e39" /> @ <img height="30" alt="Narwin" src={Narwin} style={{ verticalAlign: 'middle' }} />
+              </Link>
+            </div>
+          </div>
+        </IconContext.Provider>
       </Slide>
     )
   }
