0ee193e4a618a5dfb006a794617129656d4f6563
Patrick Lindsay <patrick.lindsay@dockyard.com>
2019-08-23 12:55:42 -0500

    Clean up WebGL slide

--- src/presentation/02-webgl.js
+++ src/presentation/02-webgl.js
@@ -18,16 +18,18 @@ export default class WebGLSlide extends React.Component {
         <Heading size={2}>What is WebGL?</Heading>
         <Appear>
           <BlockQuote>
-            <Quote textColor="dark" textSize=".8em">WebGL is a cross-platform, royalty-free web standard for a low-level 3D graphics API based on OpenGL ES, exposed to ECMAScript via the HTML5 Canvas element. Developers familiar with OpenGL ES 2.0 will recognize WebGL as a Shader-based API using GLSL, with constructs that are semantically similar to those of the underlying OpenGL ES API. It stays very close to the OpenGL ES specification, with some concessions made for what developers expect out of memory-managed languages such as JavaScript. WebGL 1.0 exposes the OpenGL ES 2.0 feature set; WebGL 2.0 exposes the OpenGL ES 3.0 API.</Quote>
+            <Quote textColor="dark" textSize=".8em">
+              WebGL is a cross-platform, royalty-free web standard for a low-level 3D graphics API based on OpenGL ES, exposed to ECMAScript via the HTML5 Canvas element. Developers familiar with OpenGL ES 2.0 will recognize WebGL as a Shader-based API using GLSL, with constructs that are semantically similar to those of the underlying OpenGL ES API. It stays very close to the OpenGL ES specification, with some concessions made for what developers expect out of memory-managed languages such as JavaScript. WebGL 1.0 exposes the OpenGL ES 2.0 feature set; WebGL 2.0 exposes the OpenGL ES 3.0 API.
+            </Quote>
             <Cite>Khronos Group, Inc.</Cite>
           </BlockQuote>
         </Appear>
-        <List textColor="dark" bulletStyle="">
+        <List textColor="dark">
           <Appear>
-            <ListItem textAlign="left">Lets the browser access the GPU</ListItem>
+            <ListItem>Lets the browser access the GPU</ListItem>
           </Appear>
           <Appear>
-            <ListItem textAlign="left">
+            <ListItem>
               Works in all major browsers
               <Link
                 href="https://caniuse.com/#feat=webgl"
